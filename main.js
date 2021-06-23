b351054630be8b02eb3976176886d7f955d85cd5
aaas <asddaaa@gmail.com>
2021-06-24 00:14:10 +0300

    end

---
+++ main.js
@@ -0,0 +1,19 @@
+var myName = prompt("Ismingizni kiriting");
+
+var Year = prompt("Yoshingizni kiriting")
+
+
+var misol1 = prompt("Misolni yeching:  4 + 6 =")
+var misol2 = prompt("Misolni yeching:  15 - 8 =")
+var misol3 = prompt("Misolni yeching:  15 % 4 =")
+var misol4 = prompt("Misolni yeching:  24 / 12 =")
+alert("Natijani ko'rish uchun konsili oching!")
+
+console.log("Mening ismim " + myName);
+console.log("Men " +Year + " yoshdaman");
+console.log("1-misol: 4 + 6 = 10" + " Sizning jovobingiz - " + misol1);
+console.log("2-misol: 15 - 8 = 7" + " Sizning jovobingiz - " + misol2);
+console.log("3-misol: 15 % 4 = 3" + " Sizning jovobingiz - " + misol3);
+console.log("4-misol: 24 / 12 = 2" + " Sizning jovobingiz - " + misol4);
+
+
